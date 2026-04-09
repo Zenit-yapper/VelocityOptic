3be3a3889c169c2750e9b4b151cda2a1bf0d00d5
Zenit-yapper <zenitog69@gmail.com>
2026-04-09 17:28:18 +0530

    Update VelocityOpticScreen.java

--- src/main/java/com/astreal/velocityoptic/VelocityOpticScreen.java
+++ src/main/java/com/astreal/velocityoptic/VelocityOpticScreen.java
@@ -1,26 +1,26 @@
 package com.astreal.velocityoptic;
 
-import net.minecraft.client.gui.Screen;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.SliderWidget;
-import net.minecraft.text.Text;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.AbstractSliderButton;
+import net.minecraft.network.chat.Component;
 
 public class VelocityOpticScreen extends Screen {
     public VelocityOpticScreen() {
-        super(Text.literal("VelocityOptic Settings"));
+        super(Component.literal("VelocityOptic Settings"));
     }
 
     @Override
     protected void init() {
         // The Slider for Strength
-        this.addDrawableChild(new SliderWidget(this.width / 2 - 100, this.height / 2 - 20, 200, 20, 
-            Text.literal("Blur Strength: " + (int)(VelocityOpticClient.blurStrength * 100) + "%"), 
+        this.addRenderableWidget(new AbstractSliderButton(this.width / 2 - 100, this.height / 2 - 20, 200, 20, 
+            Component.literal("Blur Strength: " + (int)(VelocityOpticClient.blurStrength * 100) + "%"), 
             (double) VelocityOpticClient.blurStrength) {
             
             @Override
             protected void updateMessage() {
-                this.setMessage(Text.literal("Blur Strength: " + (int)(this.value * 100) + "%"));
+                this.setMessage(Component.literal("Blur Strength: " + (int)(this.value * 100) + "%"));
             }
 
             @Override
@@ -30,15 +30,13 @@ public class VelocityOpticScreen extends Screen {
         });
 
         // Close Button
-        this.addDrawableChild(ButtonWidget.builder(Text.literal("Done"), button -> this.close())
-                .dimensions(this.width / 2 - 100, this.height / 2 + 20, 200, 20).build());
+        this.addRenderableWidget(Button.builder(Component.literal("Done"), button -> this.onClose())
+                .bounds(this.width / 2 - 100, this.height / 2 + 20, 200, 20).build());
     }
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        this.renderBackground(context, mouseX, mouseY, delta);
-        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
-        super.render(context, mouseX, mouseY, delta);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        super.render(guiGraphics, mouseX, mouseY, delta);
+        guiGraphics.drawCenteredString(this.font, this.title, this.width / 2, 20, 0xFFFFFF);
     }
 }
-
