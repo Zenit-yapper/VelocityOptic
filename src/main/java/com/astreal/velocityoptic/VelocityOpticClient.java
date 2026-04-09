b4653c5c03387f54dc840504f67626e43b78362f
Zenit-yapper <zenitog69@gmail.com>
2026-04-09 17:23:01 +0530

    Update VelocityOpticClient.java

--- src/main/java/com/astreal/velocityoptic/VelocityOpticClient.java
+++ src/main/java/com/astreal/velocityoptic/VelocityOpticClient.java
@@ -1,16 +1,29 @@
 package com.astreal.velocityoptic;
 
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class VelocityOpticClient implements ClientModInitializer {
-    public static final String MOD_ID = "velocityoptic";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static float blurStrength = 0.4f; 
+    private static KeyBinding guiKey;
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("VelocityOptic Core Loaded - Target: Minecraft 1.21.11");
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.velocityoptic.open_menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT, // Default to Right Shift
+                "category.velocityoptic"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (guiKey.wasPressed()) {
+                client.setScreen(new VelocityOpticScreen());
+            }
+        });
     }
 }
-
